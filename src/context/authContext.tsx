cf12b52c7d39eb8862aff15e6e0cb91c158c6d8d
Henrikcox <henrik.cox@gmail.com>
2023-02-27 14:41:35 -0500

    Delete src directory

--- src/context/authContext.tsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import firebase from "firebase/auth";
-
-export const AuthContext = React.createContext<firebase.User | null>(null);
--- src/py/archive/readUpdates.py
+++
@@ -1,5 +0,0 @@
-FO=open('myfile.csv', 'r')
-while True:
-    loglines=FO.readline()
-    if loglines.find('my search') >=0:
-        print(loglines)
