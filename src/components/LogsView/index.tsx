d11cbcf0465820c0f443c526aec0333029158e2e
Jordan Slott <jordan.slott@twosigma.com>
2023-03-20 17:20:22 -0400

    Add log table and basic details pane

--- src/components/LogsView/index.tsx
+++ src/components/LogsView/index.tsx
@@ -1,23 +1,52 @@
-import { useQuery } from '@tanstack/react-query';
+import * as LogRecord from 'models/LogRecord';
+import { Button } from 'components/ui/Button';
 import { LogList } from './LogList';
+import './LogsView.css';
 
-const READ_LOG_QUERY = ['allLogs'];
+// const READ_LOG_QUERY = ['allLogs'];
 
 export function LogsView(): JSX.Element {
-  const { data: logs } = useQuery({
-    queryFn: window.SentinelDesktopService.getAllLogRecords,
-    queryKey: READ_LOG_QUERY,
-  });
+  // Example log data (just for now)
+  const logs: LogRecord.T[] = [
+    {
+      id: '100',
+      level: 'INFO',
+      timestamp: new Date(),
+      message: 'This is the first log message.',
+    },
+    {
+      id: '101',
+      level: 'WARNING',
+      timestamp: new Date(),
+      message: 'This is the second log message.',
+    },
+    {
+      id: '102',
+      level: 'DEBUG',
+      timestamp: new Date(),
+      message: 'This is the third log message.',
+    },
+    {
+      id: '103',
+      level: 'ERROR',
+      timestamp: new Date(),
+      message: 'This is the fourth log message.',
+    },
+  ];
+
+  // const { data: logs } = useQuery({
+  //   queryFn: window.SentinelDesktopService.getAllLogRecords,
+  //   queryKey: READ_LOG_QUERY,
+  // });
 
   return (
     <>
-      <h1>Logs</h1>
       <div className="LogsView__log-component-wrapper">
         <LogList logs={logs} />
       </div>
-      <button type="button" className="LogsView__start-button">
-        Contact support
-      </button>
+      <div className="LogsView__start-button">
+        <Button>Report Issue</Button>
+      </div>
     </>
   );
 }
