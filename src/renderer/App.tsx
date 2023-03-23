f13a33ffef8d405972c836ad7106a80d062df571
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2023-03-23 13:19:50 -0400

    Added basic config provider and theme

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,4 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ConfigProvider } from 'antd';
+import type { ThemeConfig } from 'antd';
 import {
   MemoryRouter as Router,
   Routes,
@@ -16,73 +18,82 @@ import { AfterOrgInput } from 'pages-DEPRECATED/afterorg';
 
 const QUERY_CLIENT = new QueryClient();
 
+const THEME: ThemeConfig = {
+  token: {
+    colorPrimary: '#00aaff',
+    colorTextHeading: '#656565',
+  },
+};
+
 /**
  * Main app component with all the routes.
  */
 export default function App(): JSX.Element {
   return (
-    <div className="App">
+    <ConfigProvider theme={THEME}>
       <QueryClientProvider client={QUERY_CLIENT}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Navigate to="/run-model" />} />
-            <Route
-              path="/run-model"
-              element={
-                <Layout>
-                  <RunModelView />
-                </Layout>
-              }
-            />
+        <div className="App">
+          <Router>
+            <Routes>
+              <Route path="/" element={<Navigate to="/run-model" />} />
+              <Route
+                path="/run-model"
+                element={
+                  <Layout>
+                    <RunModelView />
+                  </Layout>
+                }
+              />
 
-            <Route
-              path="/logs"
-              element={
-                <Layout>
-                  <LogsView />
-                </Layout>
-              }
-            />
+              <Route
+                path="/logs"
+                element={
+                  <Layout>
+                    <LogsView />
+                  </Layout>
+                }
+              />
 
-            <Route
-              path="/past-results"
-              element={
-                <Layout>
-                  <PastResultsView />
-                </Layout>
-              }
-            />
+              <Route
+                path="/past-results"
+                element={
+                  <Layout>
+                    <PastResultsView />
+                  </Layout>
+                }
+              />
 
-            <Route
-              path="/more-models"
-              element={
-                <Layout>
-                  <ModelMarketplaceView />
-                </Layout>
-              }
-            />
+              <Route
+                path="/more-models"
+                element={
+                  <Layout>
+                    <ModelMarketplaceView />
+                  </Layout>
+                }
+              />
 
-            <Route
-              path="/settings"
-              element={
-                <Layout>
-                  <SettingsView />
-                </Layout>
-              }
-            />
+              <Route
+                path="/settings"
+                element={
+                  <Layout>
+                    <SettingsView />
+                  </Layout>
+                }
+              />
 
-            {/* TODO: this is a legacy route. Is it still needed? */}
-            <Route
-              path="/orgsubmitted"
-              element={
-                <Layout>
-                  <AfterOrgInput />
-                </Layout>
-              }
-            />
-          </Routes>
-        </Router>
+              {/* TODO: this is a legacy route. Is it still needed? */}
+              <Route
+                path="/orgsubmitted"
+                element={
+                  <Layout>
+                    <AfterOrgInput />
+                  </Layout>
+                }
+              />
+            </Routes>
+          </Router>
+        </div>
       </QueryClientProvider>
-    </div>
+    </ConfigProvider>
   );
 }
