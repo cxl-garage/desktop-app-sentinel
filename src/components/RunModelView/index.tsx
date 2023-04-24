e8084ff6e71c4788f4a1225a884aa2d131f344c3
tstmonger <Tristan.Monger@towsigma.com>
2023-04-24 16:43:20 -0400

    Remove extraneous change

--- src/components/RunModelView/index.tsx
+++ src/components/RunModelView/index.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import styled from 'styled-components';
 import RunModelInputs from './RunModelInputs/RunModelInputs';
 import RunModelResults from './RunModelResultsPanel/RunModelResults';
