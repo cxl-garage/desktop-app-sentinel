24350a0f2f0b23bf95dfdaf90f0c73cab97fb50a
Ang Lee <ang.lee@gmail.com>
2023-10-29 10:28:08 -0400

    Add retry when checking Docker version

--- src/components/SettingsView/hooks/useDockerVersion.ts
+++ src/components/SettingsView/hooks/useDockerVersion.ts
@@ -6,6 +6,8 @@ const useDockerVersion = (): UseQueryResult<DockerVersion.T, Error> => {
   return useQuery<DockerVersion.T, Error>({
     queryFn: () => window.SentinelDesktopService.getVersion(),
     queryKey: ['docker', 'getVersion'],
+    retry: 3,
+    retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
   });
 };
 
