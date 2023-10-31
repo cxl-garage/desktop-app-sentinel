a1f83c1168a601247baaa933e5d209b7714d9580
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2023-10-31 08:57:48 -0700

    Update getHumanFriendlyErrorMessage.ts

--- src/components/SettingsView/DockerVersionPanel/getHumanFriendlyErrorMessage.ts
+++ src/components/SettingsView/DockerVersionPanel/getHumanFriendlyErrorMessage.ts
@@ -1,6 +1,6 @@
 const getHumanFriendlyErrorMessage = (originalError: string): string => {
   if (originalError.includes('HTTP code 500')) {
-    return 'Failed to detect docker desktop. Please ensure that docker desktop is installed and running and restart the Sentinel app.';
+    return 'Failed to detect Docker Desktop. Please ensure that Docker Desktop is installed and running. If it is, please restart the Sentinel app.';
   }
   return originalError;
 };
